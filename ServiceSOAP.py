f9ae4f07d22b70c90f2229484858dddf6fa01f92
constann <nicolas.constandi@amersports.com>
2021-10-13 21:08:02 +0200

    fonction tempsParcours OK en soap, customization a faire pour calculer si charge est nécessaire ou non

--- ServiceSOAP.py
+++ ServiceSOAP.py
@@ -1,34 +1,25 @@
-from spyne import Application
-from spyne import ServiceBase
-from spyne import rpc
+from spyne import *
 from spyne.server.wsgi import WsgiApplication
 from spyne.protocol.soap import Soap11
 from spyne import *
 from math import sin, cos, acos, pi
-from requests import *
-
-class distanceGPS(ServiceBase):
-    @rpc(Float, Float, Float, Float, _returns=Iterable(Float))
-    def distanceGPS(ctx, latA, longA, latB, longB):
-        """Retourne la distance en mètres entre les 2 points A et B connus grâce à
-           leurs coordonnées GPS (en radians).
-        """
-        # Rayon de la terre en mètres (sphère IAG-GRS80)
-        RT = 6378137
-        # angle en radians entre les 2 points
-        S = acos(sin(latA)*sin(latB) + cos(latA)*cos(latB)*cos(abs(longB-longA)))
-        # distance entre les 2 points, comptée sur un arc de grand cercle
-        result=S*RT
-        yield result
-
+import requests
+import json
 class tempsParcours(ServiceBase):
-    @rpc(String, String, String, String, _returns=Iterable(Float))
-    def tempsParcours(ctx, latA, longA, latB, longB):
+    @rpc(String, String, String, String, Integer, _returns=String)
+    def tempsParcours(ctx, latA, longA, latB, longB, autonomie):
         proto="http://wxs.ign.fr/essentiels/itineraire/rest/route.json?origin=" + latA + "," + longA + "&destination=" + latB + "," + longB + "&method=DISTANCE&graphName=Voiture"
         rawdata= requests.get(proto)
-        json = json.loads(rawdata)
-        result = json["distance"]
-        yield result
+        json_loaded = rawdata.json()
+        result = json_loaded.get('duration')
+        distance = json_loaded.get('distance')
+        #if distance > autonomie:
+        #   print("Une recharge est nécessaire")
+        #else:
+        #    print("pas de recharge nécessaire")
+        print(result)
+        return result
+
             
 application = Application([tempsParcours], 'spyne.examples.hello.soap', in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())
 wsgi_application = WsgiApplication(application)
