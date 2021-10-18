e71ec4a349978f0cf1084637b1760dd76fe4fc62
constann <nicolas.constandi@amersports.com>
2021-10-18 16:44:06 +0200

    Start ignoring clientSoap.py

--- clientSoap.py
+++
@@ -1,8 +0,0 @@
-import zeep
-
-wsdl = 'http://127.0.0.1:8000/?wsdl'
-client = zeep.Client(wsdl=wsdl)
-
-result = client.service.tempsParcours("-0.6756162643432286", "45.8786949940647", "-0.34465312957759703", "46.015243021856286", "35")
-
-print(result)
