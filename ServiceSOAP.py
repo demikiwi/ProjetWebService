bd23398f8d3c18916eee0cd848b234684799c812
constann <nicolas.constandi@amersports.com>
2021-10-20 15:04:53 +0200

    création Dockerfile + Suppression runtime.txt & procfile

--- ServiceSOAP.py
+++ ServiceSOAP.py
@@ -5,15 +5,11 @@ from spyne import *
 from math import sin, cos, acos, pi
 import requests
 import json
-import os
-from flask import Flask
-
-app = Flask(__name__)
 
 class tempsParcours(ServiceBase):
     @rpc(String, String, String, String, String, _returns=String)
     def tempsParcours(ctx, latA, longA, latB, longB, autonomie):
-        proto="http://wxs.ign.fr/essentiels/itineraire/rest/route.json?origin=" + latA + "," + longA + "&destination=" + latB + "," + longB + "&method=DISTANCE&graphName=Voiture"
+        proto="http://wxs.ign.fr/essentiels/itineraire/rest/route.json?origin=" + latA + "," + longA + "&destination=" + latB + "," + longB + "&method=DISTANCE"
         rawdata= requests.get(proto)
         json_loaded = rawdata.json()
         duration = json_loaded.get('duration')
@@ -32,17 +28,7 @@ class tempsParcours(ServiceBase):
 application = Application([tempsParcours], 'spyne.examples.hello.soap', in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())
 wsgi_application = WsgiApplication(application)
 
-@app.route("/")
-def hello():
-    return wsgi_application
-
-
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
-    # from wsgiref.simple_server import make_server
-    # server = make_server('0.0.0.0', 80, wsgi_application)
-    # server.serve_forever()
+    from wsgiref.simple_server import make_server
+    server = make_server('0.0.0.0', 80, wsgi_application)
+    server.serve_forever()
